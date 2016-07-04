cc1cb7eaefacaf36f80b5abb05166461fbee7f5c
jameshu2009@163.com <jameshu2009@163.com>
2016-07-04 19:36:59 +0800

    gx

--- web.php
+++ web.php
@@ -2,13 +2,13 @@
 <body> 
 
 <h1>Search</h1>                                                                                                          
-<form action="http://59.111.109.15/" method="get">                                                      
+<form action="http://haitao2-backend:8080" method="get">                                                      
 <p>product<input type="text" name="searchName"></p>                                                                 
 <p><input type="submit"></p>                                                                                     
 </form>  
 
 <h1>Buy</h1>
-<form action="http://59.111.109.15:8080/" method="get">                                                      
+<form action="http://haitao2-backend:8080" method="get">                                                      
 <p>product<input type="text" name="buyName"></p>                                                                 
 <p><input type="submit"></p>                                                                                     
 </form>                                                                                                        
