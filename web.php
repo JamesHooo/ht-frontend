965ef30463cc1b6f30eec424c1741b0c2d0b4587
jameshu2009@163.com <jameshu2009@163.com>
2016-07-04 14:27:22 +0800

    init

---
+++ web.php
@@ -0,0 +1,16 @@
+<html>                                                                                                           
+<body> 
+
+<h1>Search</h1>                                                                                                          
+<form action="http://59.111.109.15/" method="get">                                                      
+<p>product<input type="text" name="searchName"></p>                                                                 
+<p><input type="submit"></p>                                                                                     
+</form>  
+
+<h1>Buy</h1>
+<form action="http://59.111.109.15:8080/" method="get">                                                      
+<p>product<input type="text" name="buyName"></p>                                                                 
+<p><input type="submit"></p>                                                                                     
+</form>                                                                                                        
+</body>                                                                                                          
+</html>  
